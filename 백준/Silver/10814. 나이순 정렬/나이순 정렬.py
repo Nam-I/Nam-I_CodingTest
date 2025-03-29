262f951b66e90e5c2d2d65fca38f5ce095c3dfa8
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-03-29 21:02:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 104 ms, Memory: 39600 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+
+# 입력에서 첫 번째 줄을 제외한 모든 줄을 읽어옴
+name_list = sys.stdin.readlines()[1:]
+
+# 나이를 기준으로 이름을 정렬
+# lambda 함수를 사용하여 각 줄에서 나이를 추출하고 int형으로 변환
+name_list.sort(key=lambda iD : int(iD.split()[0]))
+
+# 정렬된 이름 리스트를 출력
+# 리스트의 각 요소를 이어붙인 문자열을 출력
+print("".join(name_list))
