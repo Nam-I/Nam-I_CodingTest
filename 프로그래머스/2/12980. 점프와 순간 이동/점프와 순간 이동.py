ccd86a8b16588c21c2df8747b5a3d81d57d06272
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-03-29 21:12:33 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    
+    while n:
+    
+        if n % 2 == 0:
+            n = n//2
+        else :
+            n = n - 1
+            answer += 1
+            
+        
+
+    return answer
