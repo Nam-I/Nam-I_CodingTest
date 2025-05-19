f2035ed06d407b3e70f2298a5136ea032b48e3e1
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-05-19 20:28:21 +0900

    [Gold IV] Title: 사냥꾼, Time: 444 ms, Memory: 43448 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -12,23 +12,43 @@
 """
 
 import sys
-from bisect import bisect_left
 
-M, N, L = map(int,sys.stdin.readline().split())
+M, N, L = map(int, sys.stdin.readline().split())
+
 x_arr = sorted(list(map(int, sys.stdin.readline().split())))
-cnt = 0
+answer = 0
+
+
+def binary_search(a, b):
+    start, end = 0, M - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        
+        if x_arr[mid] == a:
+            return mid  # 사대의 x 좌표와 동물 a 좌표가 같으면 해당 사대 인덱스 반환
+        elif x_arr[mid] < a:
+            start = mid + 1  # 사대 x 좌표가 작으면 start 값을 mid+1 로 오른쪽으로 당겨옴.
+        else:
+            end = mid - 1  # 사대 x 좌표가 크면 end 값을 mid-1 로 왼쪽으로 당겨옴.
+
+    return end  # 조건에 맞는 값이 없어 start 와 end 의 위치가 역전될 경우 end 가
+    # left point(왼쪽 값)이 되므로 end 를 반환
+
 
 for _ in range(N):
     a, b = map(int, sys.stdin.readline().split())
-    if b > L:
+
+    if b > L:  # 동물의 b 좌표가 사정거리를 벗어나면 맞출 확률이 없음. 사대의 y 좌표는 0으로 고정이므로.
         continue
-    idx = bisect_left(x_arr, a)
 
-    print(f"idx: {idx}")
-    print(f"x_arr[{idx}]: {x_arr[idx]}")
-    if abs(x_arr[idx]-a)+b <= L:
-        cnt += 1
-    elif idx > 0 and abs(x_arr[idx-1]-a)+b <= L:
-        cnt += 1
+    idx = binary_search(a, b)
 
-sys.stdout.write(f"cnt: {cnt}")
+    # 가장 가까운 사대 좌표 값을 구했지만 그 값의 바로 오른쪽에 위치한 사대 좌표 값이 
+    # 동물과 더 가까운 사대의 위치가 될 때가 있음을 고려해야 한다.
+    if abs(x_arr[idx]-a)+b <= L:  # 동물이 x 좌표가 가장 가까운 사대의 사정거리에 들어오는지 확인.
+        answer += 1  # 사냥할 수 있는지만 보면 되므로 사정거리 이내면 카운트하고 더 볼 필요 없음.
+    elif idx+1 < M and abs(x_arr[idx+1]-a)+b <= L:  # 첫번째 조건에서 사정거리에 들어오지 않는다면
+        answer += 1  # 이진탐색을 통해 구한 사대 위치의 바로 오른쪽 사대에서는 동물이 사정거리에 들어오는지 확인.
+
+sys.stdout.write(f"{answer}")
