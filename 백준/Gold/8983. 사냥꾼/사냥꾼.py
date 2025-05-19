0c6adcadcd831473c09022ddb364ed616c7e4ad8
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-05-19 13:13:25 +0900

    [Gold IV] Title: 사냥꾼, Time: 264 ms, Memory: 45400 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,21 +1,15 @@
-# 백준 8983.G4.사냥꾼_파이썬 풀이
-
-import sys
-
-M, N, L = map(int, sys.stdin.readline().split())
-
-# print(f"M: {M}, N: {N}, L: {L}")
-
-x_arr = list(map(int, sys.stdin.readline().split()))
-
-answer = []
-
-for _ in range(N):
-    a, b = map(int, sys.stdin.readline().split())
-    for i in range(M):
-        if abs(x_arr[i] - a) + b <= L:
-            if [a, b] not in answer:
-                answer.append([a,b])
-
-
-sys.stdout.write(f"{len(answer)}")
+from bisect import bisect_left
+input = __import__('sys').stdin.readline
+m,n,l = map(int,input().split())
+people = sorted(list(map(int,input().split())))
+cnt = 0
+for _ in range(n):
+    x,y = map(int,input().split())
+    if y>l:continue
+    nx = bisect_left(people,x)
+    for idx in (nx,nx-1):
+        if 0<=idx<m:
+            if abs(people[idx]-x)+y<=l:
+                cnt+=1
+                break
+print(cnt)
