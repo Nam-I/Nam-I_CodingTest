40568d04cdbbc97f014d375542a4a7fa45d0dd3c
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-05-26 12:38:25 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,4 +1,4 @@
-# 백준 1629.S1.곱셉_파이썬 거듭제곱 분할정복, 모듈러 분배법칙, 재귀 풀이
+# 백준 1629.S1.곱셉_파이썬 pow() 풀이
 # 체감 난이도: 수학적인 지식이 없으면 못 푸는 문제인 것 같다. 지수를 분할한다는 힌트가 있었다면
 # 고민이라도 해봤을 수도 있는데 어떻게 접근해야할지 안떠올라서 그냥 쌩으로 틀렸다.
 # 풀이를 참고하니 이해가 된 것 같은데 다음에 이런 문제가 나온다면 못맞출 듯
@@ -13,18 +13,5 @@
 import sys
 
 A, B, C = map(int, sys.stdin.readline().split())
-
-
-def product(a, b, c):
-    if b == 1:
-        return a % c
-
-    tmp = product(a, b//2, c)
-
-    if b % 2 == 0:
-        return tmp * tmp % c
-    else:
-        return tmp * tmp * a % c
-
-
-sys.stdout.write(f"{product(A, B, C)}")
+sys.stdout.write(f"{pow(A, B, C)}")  
+# 첫번째 인자: 곱하는 수, 두번째 인자: 거듭제곱 횟수, 세번째 인자: 모듈러 연산을 수행할 수
