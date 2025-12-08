5e0b6e899387b6ae414e6b2f81ad5ec548d162ac
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-12-08 20:00:34 +0900

    [Gold IV] Title: 탈출, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,89 +1,125 @@
-/**백준 3055.G5.탈출 - java 풀이 **/
+/** 백준 3055.G5.탈출 - java 풀이 **/
+/**
+ * ## 체감 난이도 ##
+ * - 딱 G5 정도로 느껴졌다.
+ * - 구현의 큰 틀은 떠올릴 수 있었지만, size() 를 구해서 이번 타임에 갈 수 있는 곳만 확인하는 구현은 못 떠올렸다.
+ * - 디테일한 구현을 고민할 수 있는 문제다.
+ * ## 핵심 내용 ##
+ * - Queue 를 두개 사용해서 푸는 문제
+ * - 문제 조건 잘 생각해서 디테일한 구현
+ * - 나는 시작,끝 좌표쌍을 배열로 묶어서 저장하려고 했는데 코테에서는 불변 객체로 저장하는게 좋다고 하여 startR, startC 이런 식으로 저장함
+ * - 현재 큐 크기만큼만 탐색하기
+ * - 큐 중복 좌표 저장 예측해 미리 방문 표시하기
+ **/
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int R, C;
     static char[][] map;
-    static int r;
-    static int c;
-    static int[] dy = {1, 0, -1, 0};
-    static int[] dx = {0, -1, 0, 1};
+    static int startR, startC;
+    static int endR, endC;
+    static Queue<int[]> q = new LinkedList<>();
+    static Queue<int[]> water = new LinkedList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        r = sc.nextInt();
-        c = sc.nextInt();
-        map = new char[r][c];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int sY = 0;
-        int sX = 0;
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
 
-        for (int i = 0; i < r; i++) {
-            String line = sc.next();
-            for (int j = 0; j < c; j++) {
+        for (int i = 0; i < R; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < C; j++) {
                 map[i][j] = line.charAt(j);
                 if (map[i][j] == 'S') {
-                    sY = i;
-                    sX = j;
-                }
-            }
-        }
-
-        int result = bfs(sY, sX);
-        System.out.println(result == -1 ? "KAKTUS" : result);
-    }
-
-    private static int bfs(int sY, int sX) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sY, sX});
-        int[][] dis = new int[r][c];
-        dis[sY][sX] = 1;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (map[i][j] == '*') {
+                    startR = i; startC = j;
                     q.offer(new int[]{i, j});
+                    map[i][j] = '.'; // 고슴도치 출발하므로 시작 지점 빈칸으로
+                } 
+                else if (map[i][j] == 'D') {
+                    endR = i; endC = j; // 종료 지점 좌표 저장
+                } 
+                else if (map[i][j] == '*') {
+                    water.offer(new int[]{i, j}); // 물 영역 water 큐 저장
                 }
             }
         }
 
+        boolean[][] visited = new boolean[R][C];
+        visited[startR][startC] = true; // 고슴도치 시작 지점 방문 true
+
+        int time = 0;
         while (!q.isEmpty()) {
-            int[] move = q.poll();
-            char now = map[move[0]][move[1]];
-
-            for (int i = 0; i < 4; i++) {
-                int moveY = move[0] + dy[i];
-                int moveX = move[1] + dx[i];
-
-                if (validation(moveY, moveX)) {
-                    continue;
-                }
-
-                if (now == 'S') {
-                    if (map[moveY][moveX] == 'D') {
-                        return dis[move[0]][move[1]];
-                    }
-                    if (map[moveY][moveX] == '.') {
-                        map[moveY][moveX] = 'S';
-                        q.offer(new int[]{moveY, moveX});
-                        dis[moveY][moveX] = dis[move[0]][move[1]] + 1;
-                    }
-                }
-
-                if (now == '*') {
-                    if (map[moveY][moveX] == '.' || map[moveY][moveX] == 'S') {
-                        map[moveY][moveX] = '*';
-                        q.offer(new int[]{moveY, moveX});
+            
+            // 물 확장 먼저
+            int wSize = water.size(); // 이번 타임에 확장할 수 있는 영역만 계산
+            for (int i = 0; i < wSize; i++) {
+                int[] waterArea = water.poll();
+                
+                for (int d = 0; d < 4; d++) {
+                    int nx = waterArea[0] + dx[d];
+                    int ny = waterArea[1] + dy[d];
+                    
+                    if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue; // 배열 범위 밖이면 건너뜀
+                    
+                    if (map[nx][ny] == '.') { // 빈칸이면 물 범위 확장
+                        map[nx][ny] = '*';
+                        water.offer(new int[]{nx, ny});
                     }
                 }
             }
+
+            
+            // 현재 큐 크기만큼 고슴도치 이동(현재 이동 가능한 영역에서 다음 타임 이동 가능 영역 계산)
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                int[] curLocation = q.poll();
+                int curX = curLocation[0], curY = curLocation[1];
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = curX + dx[d];
+                    int ny = curY + dy[d];
+                    
+                    if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue; // 배열 범위 밖이면 건너뛰기
+                    if (visited[nx][ny]) continue; // 방문한 위치면 건너뛰기
+
+                    // 도착지로 이동 가능
+                    if (map[nx][ny] == 'D') {
+                        ++time; // 이 이동은 다음 분에 도달하므로 time+1
+                        bw.write(time + "\n");
+                        bufferedClose(); // Buffered br, bw 닫기
+                        return;
+                    }
+                    // 빈칸이면 이동 가능(이미 물로 바뀌어 있으면 불가)
+                    if (map[nx][ny] == '.') {
+                        visited[nx][ny] = true; // 큐에 중복으로 추가되는 것을 방지하기 위해 미리 다음 타임 방문 표시
+                        q.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+
+            time++;
         }
-        return -1;
+
+        bw.write("KAKTUS\n");
+        bufferedClose();
     }
 
-    private static boolean validation(int moveY, int moveX) {
-        return !(0 <= moveY && moveY < r && 0 <= moveX && moveX < c);
+    /**Buffered br, bw 닫는 메서드**/
+    private static void bufferedClose() throws Exception{
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
