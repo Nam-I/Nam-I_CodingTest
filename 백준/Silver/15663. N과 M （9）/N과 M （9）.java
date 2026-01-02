56f152280d00f65952b1a55c0e6983374a7c8c4d
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2026-01-02 18:45:50 +0900

    [Silver II] Title: N과 M (9), Time: 288 ms, Memory: 36688 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,77 @@
+/** 백준 15663.S2.N과 M(9) - LinkedHashSet 중복 제거 java 풀이
+ * ## 체감 난이도 ##
+ * - N과 M 시리즈를 꽤 풀어봤지만 이번에는 동일한 숫자가 존재하는 경우를 고려해야해서
+ * 조금 더 생각할 거리가 있었다.
+ * - LinkedHashSet 자료형의 특징을 알 수 있는 문제이다.
+ * ## 핵심 내용 ##
+ * - prev 변수를 두어 같은 depth 에서 이전에 사용한 수를 확인하는 방법이 시간/공간 측면에서 더 좋다.
+ * - 풀이에서 자동 오름차순 정렬하는 TreeSet 사용하지 않고 LinkedHashSet을 쓴 이유
+ * : 문자열 정렬이기 때문에 숫자 상 오름차순이 아니라 문자열 상 오름차순 정렬이 되어
+ * 답이 틀려진다. -> LinkedHashSet 은 입력 순서대로 정렬, 중복 제거가 가능하므로 문제에 적절
+ **/
+import java.io.*;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    static int N, M;
+    static HashSet<String> set;
+    static int[] arr;
+    static int[] resultArr;
+    static boolean[] visited;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        resultArr = new int[M];
+        visited = new boolean[N];
+        set = new LinkedHashSet<>();
+
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        backtracking(0);
+
+        for (String s: set) {
+            bw.write(s);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void backtracking(int depth) {
+        if (depth == M) {
+            sb = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                sb.append(resultArr[i]).append(" ");
+            }
+            set.add(sb.append("\n").toString());
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            resultArr[depth] = arr[i];
+            backtracking(depth + 1);
+            visited[i] = false;
+        }
+
+    }
+}
